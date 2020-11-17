3c7107f1d1021542ee15117e0e2ca90368716f66
mazdak <nazemi.works@gmail.com>
2020-11-17 11:07:03 +0000

    run ticket server normally

--- stubhub-ticket/auth/src/index.ts
+++ stubhub-ticket/auth/src/index.ts
@@ -8,15 +8,13 @@ import { app } from './app'
 
 // -----------------------------------------------------
 
-const mongoRoute = 'mongodb://auth-mongo-service:27017/auth'
-
 // -----------------------------------------------------
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY MUST BE DEFINE')
-
+  if (!process.env.MONGO_URI) throw new Error('MONGO_URI MUST BE DEFINE')
   try {
-    await mongoose.connect(mongoRoute, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
--- stubhub-ticket/tickets/src/index.ts
+++ stubhub-ticket/tickets/src/index.ts
@@ -8,25 +8,21 @@ import { app } from './app'
 
 // -----------------------------------------------------
 
-const mongoRoute = 'mongodb://tickets-mongo-service:27017/auth'
-
-// -----------------------------------------------------
-
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY MUST BE DEFINE')
-
+  if (!process.env.MONGO_URI) throw new Error('MONGO_URI MUST BE DEFINE')
   try {
-    await mongoose.connect(mongoRoute, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
     })
-    console.log('connected to auth-db')
+    console.log('connected to tickets-db')
   } catch (error) {
     console.log(error, 'herrrrrrrrrrrrrrrrrrrrrrrrrrrrrr')
   }
   app.listen(3000, () => {
-    console.log('auth-service listening on port 3000!!!!!!!!')
+    console.log('tickets-service listening on port 3000!!!!!!!!')
   })
 }
 
